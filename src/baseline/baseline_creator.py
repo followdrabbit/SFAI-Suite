e702153643d082be432e130f7e73996aa3fdb0f4
Raphael C. Florencio <raphael.tecsp@gmail.com>
2024-02-03 23:11:00 -0300

    Get and save data working

--- src/baseline/baseline_creator.py
+++ src/baseline/baseline_creator.py
@@ -1,160 +1,125 @@
-# Imports
-from utils.thread_manager import OpenAIThreadManager  # Importing OpenAIThreadManager for thread management.
-from utils.text_replacer import replace_text_in_file  # Importing function to replace text in files.
-from utils.assistant_manager import OpenAIAssistantManager  # Importing OpenAIAssistantManager for managing assistants.
-from utils.runs_manager import OpenAIRunsManager  # Importing OpenAIRunsManager for managing runs.
+# Imports corrigidos conforme os nomes fornecidos originalmente
+import json
+import os
+from utils.thread_manager import OpenAIThreadManager
+from utils.assistant_manager import OpenAIAssistantManager
+from utils.runs_manager import OpenAIRunsManager
+from utils.text_replacer import replace_text_in_file
 
-# Constants
-CLOUD_SECURITY_EXPERT = "Cloud Security Expert"  # Constant for a specific assistant role.
-PRODUCT_NAME_PLACEHOLDER = "PRODUCT_NAME"  # Placeholder for product name in prompts.
-CONTROL_NAME_PLACEHOLDER = "CONTROL_NAME"  # Placeholder for control name in prompts.
-DATA_RAW_DIR = "data/raw"  # Directory path for raw data.
-DATA_STRUCTURED_DIR = "data/structured"  # Directory path for structured data.
-GET_BASELINE_CONTROLS_PROMPT = 'prompts/get_baseline_controls.txt'  # File path for baseline controls prompt.
-
+# Constantes
+CLOUD_SECURITY_EXPERT = "Cloud Security Expert"
+PRODUCT_NAME_PLACEHOLDER = "PRODUCT_NAME"
+CONTROL_NAME_PLACEHOLDER = "CONTROL_NAME"
+DATA_RAW_DIR = "data/raw"
+DATA_STRUCTURED_DIR = "data/structured"
+GET_BASELINE_CONTROLS_PROMPT = 'prompts/get_baseline_controls.txt'
+BASELINE_AUDIT_PROMPT = 'prompts/get_baseline_audit.txt'
+BASELINE_REMEDIATION_PROMPT = 'prompts/get_baseline_remediation.txt'
+BASELINE_REFERENCE_PROMPT = 'prompts/get_baseline_reference.txt'
 
 async def find_assistant_id(assistant_manager, name):
-    """Find assistant ID based on its name. This is an asynchronous function."""
-    unique_assistants = await assistant_manager.list_assistants()  # List all assistants.
-    return unique_assistants.get(name)  # Return the ID of the assistant with the given name.
+    """Returns the ID of an assistant by name."""
+    unique_assistants = await assistant_manager.list_assistants()
+    return unique_assistants.get(name)
 
-async def create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, prompt_file, placeholder, replacement_text):
-    """Create and process a run for a specific placeholder replacement in a prompt. This is an asynchronous function."""
-    new_prompt = replace_text_in_file(prompt_file, placeholder, replacement_text)  # Replace placeholder in prompt file.
-    print("Getting controls")
-    await thread_manager.create_message(thread_id, new_prompt)  # Create a message in the thread.
-    run_id = await runs_manager.create_run(thread_id, assistant_id)  # Create a run with the thread ID and assistant ID.
-    return await runs_manager.process_run(thread_id, run_id)  # Process the run and return the result.
+async def create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, prompt_file, placeholder, replacement_text, process):
+    """Creates and processes a run for replacing placeholders in a prompt."""
+    new_prompt = replace_text_in_file(prompt_file, placeholder, replacement_text)
+    print(f"Getting {process} for: {replacement_text}")
+    await thread_manager.create_message(thread_id, new_prompt)
+    run_id = await runs_manager.create_run(thread_id, assistant_id)
+    return await runs_manager.process_run(thread_id, run_id)
 
 def extract_response(result_raw):
-    """
-    Extract assistant messages from the raw response.
-
-    Args:
-    result_raw (list): A list or a list of lists containing dictionaries with 'role' and 'message' keys.
-
-    Returns:
-    str: A concatenated string of all assistant messages.
-    """
+    """Extracts assistant messages from the raw response."""
     messages = []
-
-    try:
-        # Check if result_raw is a list of lists or a simple list
-        if result_raw and isinstance(result_raw[0], list):
-            # It's a list of lists: iterate over each sublist
-            for sublist in result_raw:
-                # Iterate over each item (dictionary) in the sublist
-                for item in sublist:
-                    # Check if the item is a dictionary and the role is 'assistant'
-                    if isinstance(item, dict) and item.get('role') == 'assistant':
-                        # Append the message of the assistant to the messages list
-                        messages.append(item.get('message', ''))
-        else:
-            # It's a simple list: iterate directly over result_raw
-            for item in result_raw:
-                # Check if the item is a dictionary and the role is 'assistant'
-                if isinstance(item, dict) and item.get('role') == 'assistant':
-                    # Append the message of the assistant to the messages list
-                    messages.append(item.get('message', ''))
-
-    except Exception as e:
-        # Print an error message if an exception occurs
-        print(f"An error occurred while processing the response: {e}")
-        # Optional: Return an error message or an empty list depending on how you want to handle errors
-        # return "Error processing response"
-        # or
-        # return []
-
-    # Join all messages in the list into a single string separated by spaces
+    for item in (item for sublist in result_raw for item in (sublist if isinstance(sublist, list) else [sublist])):
+        if item.get('role') == 'assistant':
+            messages.append(item.get('message', ''))
     return " ".join(messages)
 
 async def process_control_blocks(thread_manager, thread_id, runs_manager, assistant_id, controls_extracted):
+    """Processes control blocks for audit and remediation."""
     all_controls = {}
-    prompt_files = ['prompts/get_baseline_audit.txt', 'prompts/get_baseline_remediation.txt']
-    for prompt_file in prompt_files:  # Itera sobre a lista de valores prompt_file
-        # Define o valor de 'process' com base em 'prompt_file'
-        if prompt_file == 'prompts/get_baseline_audit.txt':
-            process = 'Audit'
-        elif prompt_file == 'prompts/get_baseline_remediation.txt':
-            process = 'Remediation'
-        controls = {}
-        control_counter = 1
-        current_control_block = []
-        last_processed_block = None
-
+    for prompt_file, process in [(BASELINE_AUDIT_PROMPT, 'Audit'), (BASELINE_REMEDIATION_PROMPT, 'Remediation'), (BASELINE_REFERENCE_PROMPT, 'Reference')]:
+        controls, control_counter = {}, 1
+        current_control_block, last_processed_block = [], None
         for line in controls_extracted.strip().split('\n'):
             if line.strip():
                 current_control_block.append(line.strip())
             else:
                 if current_control_block and current_control_block != last_processed_block:
+                    # Processa o bloco de controle atual
                     control_description = ' '.join(current_control_block)
-                    print(f"Getting {process} for block: {control_description}")
                     control_key = f"Control{control_counter}"
-                    controls[control_key] = {
-                        "Description": control_description,
-                    }
-                    response = await create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, prompt_file, CONTROL_NAME_PLACEHOLDER, control_description)
-                    result = next((item['message'] for item in response if item['role'] == 'assistant' and 'message' in item), None)
+                    response = await create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, prompt_file, CONTROL_NAME_PLACEHOLDER, control_description, process)
+                    result = next((item['message'] for item in response if item['role'] == 'assistant'), None)
                     if result:
-                        print(result)
-                        controls[control_key][process] = result
+                        controls[control_key] = {"Description": control_description, process: result}
                         control_counter += 1
                     last_processed_block = current_control_block.copy()
                     current_control_block = []
 
+        # Processa o último bloco de controle se ele não foi processado
         if current_control_block and current_control_block != last_processed_block:
             control_description = ' '.join(current_control_block)
-            print(f"Getting {process} for block: {control_description}")
-            response = await create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, prompt_file, CONTROL_NAME_PLACEHOLDER, control_description)
-            result = next((item['message'] for item in response if item['role'] == 'assistant' and 'message' in item), None)
+            control_key = f"Control{control_counter}"
+            response = await create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, prompt_file, CONTROL_NAME_PLACEHOLDER, control_description, process)
+            result = next((item['message'] for item in response if item['role'] == 'assistant'), None)
             if result:
-                print(result)
-                control_key = f"Control{control_counter}"
-                controls[control_key] = {
-                    "Description": control_description,
-                }
-                controls[control_key][process] = result
-                control_counter += 1
-
-        all_controls[prompt_file] = controls  # Armazena os controles para cada prompt_file
-
+                controls[control_key] = {"Description": control_description, process: result}
+        
+        all_controls[process] = controls
     return all_controls
 
+def save_data(data, ticket, base_dir="data/raw/baseline"):
+    """
+    Tenta salvar os dados em um arquivo JSON no diretório especificado.
+    Verifica se o arquivo foi salvo com sucesso e lida com possíveis erros.
 
-
+    Args:
+    - data: Os dados a serem salvos (dicionário).
+    - ticket: Identificador único para os dados.
+    - base_dir: Diretório base onde os dados serão salvos.
+    """
+    try:
+        
+        # Define o caminho completo do arquivo
+        file_path = os.path.join(base_dir, f"{ticket}.json")
+        
+        # Salva os dados no arquivo JSON
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        
+        # Verifica se o arquivo foi de fato criado
+        if os.path.exists(file_path):
+            print(f"Dados salvos com sucesso em: {file_path}")
+        else:
+            # Isso não deveria acontecer a menos que o arquivo seja deletado
+            # ou movido após ser criado mas antes desta verificação
+            print("O arquivo não foi encontrado após a tentativa de salvamento.")
+    except Exception as e:
+        # Captura qualquer exceção que possa ocorrer e exibe a mensagem de erro
+        print(f"Ocorreu um erro ao tentar salvar os dados: {e}")
 
 async def create_baseline(technology, api_key, ticket):
-    """Main function to create a baseline using specified technology, API key, and ticket. This is an asynchronous function."""
-    assistant_manager = OpenAIAssistantManager(api_key)  # Initialize the assistant manager with the API key.
-    assistant_id = await find_assistant_id(assistant_manager, CLOUD_SECURITY_EXPERT)  # Find the ID of the Cloud Security Expert.
-
-    # Check if the assistant ID was found, if not, print a message and return.
+    """Main function to create a baseline for a given technology."""
+    assistant_manager = OpenAIAssistantManager(api_key)
+    assistant_id = await find_assistant_id(assistant_manager, CLOUD_SECURITY_EXPERT)
     if assistant_id is None:
         print("Assistant 'Cloud Security Expert' not found.")
         return
 
-    thread_manager = OpenAIThreadManager(api_key)  # Initialize the thread manager with the API key.
-    runs_manager = OpenAIRunsManager(api_key)  # Initialize the runs manager with the API key.
+    thread_manager, runs_manager = OpenAIThreadManager(api_key), OpenAIRunsManager(api_key)
+    thread_id = await thread_manager.create_thread()
+    controls_raw = await create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, GET_BASELINE_CONTROLS_PROMPT, PRODUCT_NAME_PLACEHOLDER, technology, "Controls")
+    controls_extracted = extract_response(controls_raw)
 
-    thread_id = await thread_manager.create_thread()  # Create a new thread.
-    controls_raw = await create_and_process_run(thread_manager, thread_id, runs_manager, assistant_id, GET_BASELINE_CONTROLS_PROMPT, PRODUCT_NAME_PLACEHOLDER, technology)
-    controls_extracted = extract_response(controls_raw)  # Extract controls from the raw response.
+    print("##################################################################")
+    print(controls_extracted)
+    print("##################################################################")
 
-    # Process audit for the extracted controls.
-    #Teste = await process_control_block(thread_manager, thread_id, runs_manager, assistant_id, controls_extracted, GET_BASELINE_AUDIT_PROMPT, "audit")
-    # Para usar a função modificada:
-    Teste = await process_control_blocks(thread_manager, thread_id, runs_manager, assistant_id, controls_extracted)
-    print("#####################################################################")
-    print("#####################################################################")
-    print("#####################################################################")
-    print(Teste)
-    # Process remediation for the extracted controls.
-    #Teste = await process_control_block(thread_manager, thread_id, runs_manager, assistant_id, controls_extracted, GET_BASELINE_REMEDIATION_PROMPT, "remediation")
+    processed_controls = await process_control_blocks(thread_manager, thread_id, runs_manager, assistant_id, controls_extracted)
+    print(processed_controls)
 
-
-    # response = await thread_manager.list_messages(thread_id)
-    # for message in reversed(response.data):
-    #     print(message.content[0].text.value)
-    #     print()
-    #     print()
-
+    save_data(processed_controls, ticket)
